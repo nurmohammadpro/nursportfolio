9e6b1a43587346a07c3058c5c8a49a76fa83931c
Nur Mohammad <nur_mohammad1@outlook.com>
2026-01-30 06:29:26 +0600

    removed opennext

--- open-next.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-export default defineCloudflareConfig({
-  // This is the default configuration for OpenNext on Cloudflare.
-  // You can add incrementalCache or other overrides here later.
-});
